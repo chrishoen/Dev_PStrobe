cd64d30df5e33a8a69b5e514f37cd2964a8fd878
chrishoen <chris123hoen@gmail.com>
2019-09-19 11:13:20 -0400

    added pruShare

--- PStrobe/main.c
+++ PStrobe/main.c
@@ -1,21 +1,24 @@
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
+#include "pruShare.h"
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
 void main()
 {
+   // Initialize the pru shared memory region.
+   initializePruShare();
+
 	// GPIO bit 5.
 	volatile uint32_t gpio = 0x0020;
    
    // Enable the OCP master port.
    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-   // Counter pointer in shared memory section.
-   int* tCounterPtr = (int*)0x10000;
-   *tCounterPtr = 1000;
+   // Counter pointer in shared memory region.
+   gPruShare->mN1 = 2001;
    
    // Loop to toggle the gpio pin.
 	while(1){
@@ -23,4 +26,3 @@ void main()
 		__delay_cycles(200000);
 	}
 }
-
