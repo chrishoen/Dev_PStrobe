26712a0ea9e1ac3d90567ec0f33183fea4a47fde
chrishoen <chris123hoen@gmail.com>
2019-09-20 20:09:16 -0400

    more cycle counter

--- PStrobe/main.c
+++ PStrobe/main.c
@@ -7,45 +7,42 @@
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
-// Cycle counter reload value. The cycle counter will reach 
-// 0xffffffff after 1ms.
-static const unsigned cCycleReload = 0xffffffff - 200*1000;
+// Cycle counter limit for 1ms. 
 static const unsigned cCycleLimit = 200*1000;
 
 void main()
 {
-   // Initialize the pru shared memory region.
-   initializePruShare();
+   // Local variables.
 
 	// GPIO bit 5.
 	volatile uint32_t gpio = 0x0020;
-   
+
+   // Counters.
+   int tCount1ms = 0;   
+   int tCount1sec = 0;   
+
+   // Initialize the pru shared memory region.
+   initializePruShare();
+
    // Enable the OCP master port.
    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-   // Reload the cycle counter for 1ms.
+   // Clear the cycle counter.
    PRU0_CTRL.CTRL_bit.CTR_EN = 0;
    PRU0_CTRL.CYCLE = 0;
    PRU0_CTRL.CTRL_bit.CTR_EN = 1;
 
-
-   // Update shared memory region.
-   gPruShare->mN1 = 2001;
-   
    // Loop to toggle the gpio pin at 1ms.
 	while(1)
-   {
-      // Update shared memory region.
+   {     
+      // Update shared memory.
       gPruShare->mU1++;
 
-      // Get the cycle counter.
-      unsigned tCycleCount = PRU0_CTRL.CYCLE;
-      gPruShare->mU2 = tCycleCount;
-
-      // Test for 1ms.
-      if (tCycleCount > cCycleLimit)
+      // Test if the cycle counter has reached 1ms.
+      if (PRU0_CTRL.CYCLE > cCycleLimit)
       {
-         // Reload the cycle counter for 1ms.
+         // Clear the cycle counter. This will cause this section
+         // of code to execute again in 1ms.
          PRU0_CTRL.CTRL_bit.CTR_EN = 0;
          PRU0_CTRL.CYCLE = 0;
          PRU0_CTRL.CTRL_bit.CTR_EN = 1;
@@ -53,20 +50,16 @@ void main()
          // Toggle the gpio pin.
    		__R30 ^= gpio;
 
-         // Update shared memory region.
-         gPruShare->mU3++;
+         // Update shared memory.
+         gPruShare->mU2++;
+
+         // Update counters.
+         if (++tCount1ms % 1000 == 0)
+         {
+            tCount1sec++;
+         }
+         gPruShare->mU3 = tCount1ms;
+         gPruShare->mU4 = tCount1sec;
       }
 	}
 }
-
-#if 0
-   // Loop to toggle the gpio pin.
-	while(1)
-   {
-		__R30 ^= gpio;
-		__delay_cycles(200000);
-      // Update shared memory region with the cycle counter.
-      gPruShare->mU1 = PRU0_CTRL.CYCLE;
-	}
-#endif
-
