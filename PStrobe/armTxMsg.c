ea0ec0334c92a783ba67990f515746d94a22e48c
chrishoen <chris123hoen@gmail.com>
2019-09-22 19:16:53 -0400

    renaming variables

--- PStrobe/armTxMsg.c
+++ PStrobe/armTxMsg.c
@@ -24,7 +24,7 @@ volatile register uint32_t __R31;
  * PRU1 uses system event 18 (To ARM) and 19 (From ARM)
  */
 #define TO_ARM_HOST			16
-#define FROM_ARM_HOST			17
+#define FROM_ARM_HOST      17
 
 /*
  * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
@@ -44,8 +44,9 @@ volatile register uint32_t __R31;
 #define RPMSG_BUF_HEADER_SIZE           16
 uint8_t payload[RPMSG_BUF_SIZE - RPMSG_BUF_HEADER_SIZE];
 
-struct pru_rpmsg_transport transport;
-uint16_t src, dst, len;
+struct pru_rpmsg_transport rTransport;
+uint16_t rSource, rDestin, rLength;
+unsigned rSeqNum = 0;
 volatile uint8_t *status;
 
 //******************************************************************************
@@ -76,13 +77,13 @@ void initializeArmTxMsg()
    gPruShare->mArmTxMsgState = 2;
 
    /* Initialize the RPMsg transport structure */
-   pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
+   pru_rpmsg_init(&rTransport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
 
    // Update shared memory.
    gPruShare->mArmTxMsgState = 3;
 
    /* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
-   while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+   while (pru_rpmsg_channel(RPMSG_NS_CREATE, &rTransport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
 
    // Update shared memory.
    gPruShare->mArmTxMsgState = 4;
@@ -98,17 +99,14 @@ void initializeArmTxMsg()
          // Clear the event status.
          CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
 
-         // Sequence number variable for receive.
-         unsigned tSeqNum = 0;
-
          // Receive all available messages, multiple messages can be sent per kick.
-         while (pru_rpmsg_receive(&transport, &src, &dst, &tSeqNum, &len) == PRU_RPMSG_SUCCESS) 
+         while (pru_rpmsg_receive(&rTransport, &rSource, &rDestin, &rSeqNum, &rLength) == PRU_RPMSG_SUCCESS) 
          {
             // Echo the message back to the same address from which we just received.
-            pru_rpmsg_send(&transport, dst, src, &tSeqNum, len);
+            pru_rpmsg_send(&rTransport, rDestin, rSource, &rSeqNum, 4);
             gPruShare->mArmTxMsgCount++;
-            tSeqNum++;
-            pru_rpmsg_send(&transport, dst, src, &tSeqNum, len);
+            rSeqNum++;
+            pru_rpmsg_send(&rTransport, rDestin, rSource, &rSeqNum, 4);
             // Update shared memory.
             gPruShare->mArmTxMsgCount++;
          }
@@ -117,6 +115,9 @@ void initializeArmTxMsg()
          gPruShare->mArmTxMsgState = 6;
       }
 	}
+
+   // Update shared memory.
+   gPruShare->mArmTxMsgState = 7;
 }
 
 //******************************************************************************
@@ -126,7 +127,7 @@ void initializeArmTxMsg()
 
 void sendArmTxMsg(int aCode)
 {
-	pru_rpmsg_send(&transport, dst, src, payload, len);
+   pru_rpmsg_send(&rTransport, rDestin, rSource, &rSeqNum, 4);
 }
 
 //******************************************************************************
